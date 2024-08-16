6405463de016e699a274d20992980b3750e72524
mojtaba <mojtaba.mohamed187@gmail.com>
2024-08-16 20:59:45 +0300

    feat: Implement collapsible sidebar component
    - Added functionality to collapse/expand sidebar on hover
    - Styled sidebar items and added icons using React Icons
    - Implemented smooth transitions for sidebar text visibility
    
    AST-5
    #AST-5

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,8 +1,49 @@
-/**
- * TODO: Implement sidebar component
- * TODO: The sidebar should be a component that can be used in multiple places
- * TODO: The sidebar should be collapsed by default and expandable on hover
- * TODO: The sidebar items should show based on the current user type
- * TODO: The sidebar items should be clickable and navigate to the appropriate page
- * TODO: The the current page should be highlighted in the sidebar
-*/
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaLaptop, FaUser } from "react-icons/fa";
+import '../styles/Sidebar.css';
+import logo from "../assets/logo.svg";
+
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const [activeItem, setActiveItem] = useState('home');
+
+  return (
+    <div
+    className={`sidebar ${collapsed ? 'collapsed' : ''}`}
+    onMouseEnter={() => setCollapsed(false)}
+    onMouseLeave={() => setCollapsed(true)}
+  >
+      <div className="sidebar-logo">
+        <img src={logo} alt="AssetSync Logo" />
+      </div>
+      <div
+        className={`sidebar-item ${activeItem === 'home' ? 'active' : ''}`} 
+        onClick={() => setActiveItem('home')}
+      >
+          <Link to="/Home">
+          <FaHome className="icon" /> {collapsed ? '' : <span className="sidebar-text">Home</span>}
+        </Link>
+      </div>
+      <div
+        className={`sidebar-item ${activeItem === 'requests' ? 'active' : ''}`} 
+        onClick={() => setActiveItem('requests')}
+      >
+          <Link to="/requests">
+          <FaLaptop className="icon" /> {collapsed ? '' : <span className="sidebar-text">My Requests</span>}
+        </Link>
+      </div>
+      <div
+        className={`sidebar-item ${activeItem === 'profile' ? 'active' : ''}`} 
+        onClick={() => setActiveItem('profile')}
+      >
+          <Link to="/profile">
+          <FaUser className="icon" /> {collapsed ? '' : <span className="sidebar-text">Profile</span>}
+        </Link>
+      </div>
+  </div>
+  );
+
+}
+
+export default Sidebar;
