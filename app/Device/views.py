c235de1ea5a51c5399d334abfec6b06e6fea5646
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-08-23 07:47:39 +0300

    AST-3 add missing files

--- app/Device/views.py
+++ app/Device/views.py
@@ -1,4 +1,4 @@
-from octo import logging
+# from octo import logging
 from rest_framework import generics, permissions
 from ..utils.permissions import IsSuperUser, ModelPermissions
 from rest_framework.permissions import AllowAny
@@ -8,7 +8,7 @@ from rest_framework import status
 from app.device.models import Device
 from .serializers import DeviceHardwareDataSerializer
 
-log = logging.Logger(__name__).get()
+# log = logging.Logger(__name__).get()
 
 
 class DeviceListView(generics.ListCreateAPIView):
@@ -32,14 +32,14 @@ class DeviceHardwareRecevier(APIView):
     def post(self, request, *args, **kwargs):
         serializer = DeviceHardwareDataSerializer(data=request.data)
         if serializer.is_valid():
-            log.debug(f"Received serial_number: {serializer.validated_data['serial_number']}")
+            # log.debug(f"Received serial_number: {serializer.validated_data['serial_number']}")
             device = Device.objects.filter(serial_number=serializer.validated_data['serial_number']).first()
             if device:
-                log.debug("Device already exists")
+                # log.debug("Device already exists")
                 return Response({"message": "Device already exists"}, status=status.HTTP_200_OK)
             else:
                 # Create a new device if the serial number does not exist
                 Device.objects.create(**serializer.validated_data)
-                log.debug("New device created")
+                # log.debug("New device created")
                 return Response({"message": "New device created"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
