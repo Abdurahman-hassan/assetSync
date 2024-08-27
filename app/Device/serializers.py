fd27bbd030d0a0e43ea8b883a68bb3dc677ab51a
abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-08-27 06:44:08 +0300

    hot fix, show id (uuid)

--- app/Device/serializers.py
+++ app/Device/serializers.py
@@ -7,7 +7,7 @@ class DeviceHardwareDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
         fields = [
-            'hostname', 'serial_number', 'manufacturer', 'model',
+            "id", 'hostname', 'serial_number', 'manufacturer', 'model',
             'os_type', 'os_version', 'cpu', 'cpu_cores',
             'cpu_threads', 'ram_total_gb', 'disk_total_gb',
             'photo_url', 'status'
